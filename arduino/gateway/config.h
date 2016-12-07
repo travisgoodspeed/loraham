0b1650ad68b5ada70b52dbf1cef09bc06b7f51cb
Sharp Hall <sharp@sauropod.org>
2016-12-06 23:59:59 -0500

    Use m0's deep sleep mode
    This commit adds support for the m0's RTC. We can use the RTC to
    schedule wakeups. We also use the interrupt from the radio to wake up
    the controller.
    
    Also included in this commit are options in config.h for selecting the
    behavior of the LED. The LED can be set to turn on when the controller
    is woken up, or when the radio is transmitting.

--- arduino/gateway/config.h
+++ arduino/gateway/config.h
@@ -3,8 +3,12 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define CALLSIGN "AB3TL-5"
+#define CALLSIGN "YOURCALL-1"
 #define FEATHER_M0 // hardware type
+
+#if CALLSIGN == "YOURCALL-1"
+#error Change your callsign!
+#endif
 
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 434.0
@@ -12,12 +16,19 @@
 // LED to use
 #define LED 13
 
+// LED debug options
+// #define DEBUG_LED_XMIT
+#define DEBUG_LED_RTC
+
+// use RTC
+#define RTC_ENABLED
+
 // Periodic beacon enable
 #define BEACON_PERIODIC
-#define BEACON_PERIOD 15 * 1000 // ms
-#define BEACON_PERIOD_LOWBATT 30 * 1000 // ms
+#define BEACON_PERIOD 60 * 10 * 1000 // ms
+#define BEACON_PERIOD_LOWBATT 60 * 20 * 1000 // ms
 
-#define LOWBATT_WAIT_PERIOD 30 * 1000 // ms
+#define LOWBATT_WAIT_PERIOD 60 * 1000 // ms
 
 // ring buffer size in packets
 #define BUFFER_PACKETS 10
@@ -31,3 +42,4 @@
 #define ONLY_CHARGE_VOLTAGE 3.6
 
 #endif // CONFIG_H_
+
